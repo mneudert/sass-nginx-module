608017b05d191699628c2aad5abfa9010d043561
Lawrence Meckan <media@absalom.biz>
2015-09-24 21:17:23 +1000

    Standardise to context - map file

--- src/ngx_http_sass_module.c
+++ src/ngx_http_sass_module.c
@@ -67,7 +67,7 @@ static ngx_command_t  ngx_http_sass_commands[] = {
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
       ngx_conf_set_str_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
-      offsetof(ngx_http_sass_loc_conf_t, map_file),
+      offsetof(ngx_http_sass_loc_conf_t, source_map_file),
       NULL },
 
     { ngx_string("sass_output"),
