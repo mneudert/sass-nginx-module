4bc0c3717172d8f9b2aa3d34cb2c6aee6864e1b4
Lawrence Meckan <media@absalom.biz>
2015-09-24 21:15:08 +1000

    Refactor to standardised context + precision
    Refactor to standardised Libsass & add precision into build sequence

--- src/ngx_http_sass_module.c
+++ src/ngx_http_sass_module.c
@@ -11,11 +11,11 @@ typedef struct {
     ngx_uint_t  output_style;
     ngx_flag_t  source_comments;
     ngx_uint_t  precision;
-    ngx_flag_t  map_embed;
+    ngx_flag_t  source_map_embed;
     ngx_flag_t  omit_map_url;
-    ngx_flag_t  map_contents;
-    ngx_str_t   map_root;
-    ngx_str_t   map_path;
+    ngx_flag_t  source_map_contents;
+    ngx_str_t   source_map_root;
+    ngx_str_t   source_map_file;
 
 } ngx_http_sass_loc_conf_t;
 
@@ -63,11 +63,11 @@ static ngx_command_t  ngx_http_sass_commands[] = {
       offsetof(ngx_http_sass_loc_conf_t, precision),
       NULL },
 
-    { ngx_string("sass_map_path"),
+    { ngx_string("sass_map_file"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
       ngx_conf_set_str_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
-      offsetof(ngx_http_sass_loc_conf_t, map_path),
+      offsetof(ngx_http_sass_loc_conf_t, map_file),
       NULL },
 
     { ngx_string("sass_output"),
@@ -166,6 +166,7 @@ ngx_http_sass_handler(ngx_http_request_t *r)
 
     options.output_style    = clcf->output_style;
     options.source_comments = clcf->source_comments;
+    options.precision = (int *) clcf->precision.data;
     options.include_paths   = (char *) clcf->include_paths.data;
 
     ctx             = sass_new_file_context();
@@ -242,9 +243,9 @@ ngx_http_sass_create_loc_conf(ngx_conf_t *cf)
     conf->output_style    = SASS_STYLE_NESTED;
     conf->precision       = NGX_CONF_UNSET;
     conf->source_comments = NGX_CONF_UNSET;
-    conf->map_embed       = NGX_CONF_UNSET;
-    conf->omit_map_url    = NGX_CONF_UNSET;
-    conf->map_contents    = NGX_CONF_UNSET;
+    conf->source_map_embed    = NGX_CONF_UNSET;
+    conf->omit_source_map_url = NGX_CONF_UNSET;
+    conf->source_map_contents = NGX_CONF_UNSET;
 
     return conf;
 }
@@ -261,11 +262,11 @@ ngx_http_sass_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_conf_merge_uint_value(conf->output_style, prev->output_style, SASS_STYLE_NESTED);
     ngx_conf_merge_uint_value(conf->precision, prev->precision, 0);
     ngx_conf_merge_off_value(conf->source_comments, prev->source_comments, 0);
-    ngx_conf_merge_off_value(conf->map_embed, prev->map_embed, 0);
-    ngx_conf_merge_off_value(conf->omit_map_url, prev->omit_map_url, 0);
-    ngx_conf_merge_off_value(conf->map_contents, prev->map_contents, 0);
-    ngx_conf_merge_str_value(conf->map_root, prev->map_root, "");
-    ngx_conf_merge_str_value(conf->map_path, prev->map_path, "");
+    ngx_conf_merge_off_value(conf->source_map_embed, prev->source_map_embed, 0);
+    ngx_conf_merge_off_value(conf->omit_source_map_url, prev->omit_source_map_url, 0);
+    ngx_conf_merge_off_value(conf->source_map_contents, prev->source_map_contents, 0);
+    ngx_conf_merge_str_value(conf->source_map_root, prev->source_map_root, "");
+    ngx_conf_merge_str_value(conf->source_map_file, prev->source_map_file, "");
 
     return NGX_CONF_OK;
 }
